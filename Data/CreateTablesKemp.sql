0cbc25eb74ce98df1a83dd2de192deeef2f2e60f
JKemp119 <jr.kemp@proton.me>
2026-02-12 12:51:54 -0500

    Updated ConferenceDivison and Team tables

--- Data/CreateTablesKemp.sql
+++ Data/CreateTablesKemp.sql
@@ -1,2 +1,34 @@
+-- Create a database for NFL app
+
+-- use master;
+
+-- create database MIST353_NFL_RDB_Kemp;
+
+-- drop database NFL_RDB_Kemp;
+
+use MIST353_NFL_RDB_Kemp;
+
 -- Create tables for first iteration
+
+create TABLE ConferenceDivision (
+    ConferenceDivisionID int identity(1,1)
+        CONSTRAINT PK_ConferenceDivisionID PRIMARY KEY,
+    Conference NVARCHAR(50) not null,
+        CONSTRAINT CHK_Conference CHECK (Conference IN ('AFC', 'NFC')),
+    Division NVARCHAR(50) not null
+        CONSTRAINT CHK_Division CHECK (Division IN ('East', 'North', 'South', 'West'))
+);
+
+GO
+
+create table Team (
+    TeamID int identity(1,1)
+        CONSTRAINT PK_TeamID PRIMARY KEY,
+    TeamName NVARCHAR(50) not null,
+    TeamCityState NVARCHAR(50) not null,
+    TeamColors NVARCHAR(50) not null,
+    ConferenceDivisionID int not null,
+        CONSTRAINT FK_Team_ConferenceDivisionID FOREIGN KEY (ConferenceDivisionID) REFERENCES ConferenceDivision(ConferenceDivisionID)
+);
+
 
---
+++ Data/InsertDataKemp.sql
@@ -0,0 +1,4 @@
+-- Insert Data
+-- Insert all the conferenceDivision data (8 rows)
+-- Insert team data for AFC North (4 rows)
+
