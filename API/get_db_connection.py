2b162c23b5b8b4a89582229036226870918ece70
JKemp119 <jr.kemp@proton.me>
2026-04-29 19:26:56 -0400

    Changes from 4/29/2026 class

--- API/get_db_connection.py
+++ API/get_db_connection.py
@@ -1,4 +1,5 @@
 import os
+#import pyodbc
 import pymssql
 from dotenv import load_dotenv
 
@@ -10,10 +11,12 @@ def get_db_connection():
     username = os.getenv('DB_LOGIN')
     password = os.getenv('DB_PASSWORD')
 
-
-
-
     #connection_string = f"Driver={{ODBC Driver 18 for SQL Server}};Server={server};Database={database};UID={username};PWD={password};"
+    #return pyodbc.connect(connection_string)
 
 
-    return pymssql.connect(server=server, user=username, password=password, database=database, port=1433) #tds_version='7.4'
+    return.pymssql.connect(server=server, user=username, password=password, database=database, port=1433, tds_version='7.4')
+
+
+#import pyodbc
+#print(pyodbc.drivers())
